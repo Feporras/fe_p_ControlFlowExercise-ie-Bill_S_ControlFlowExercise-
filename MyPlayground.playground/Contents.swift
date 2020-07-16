0671be0f0250e5f568fefd5505c2761e50c6b2e9
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-15 21:50:14 -0500

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+for index in 1...180 {
+    print("second elapsed ---> \(index)")
+    
+    if index <= 90 {
+        print("still cooking")
+    }
+    
+    if index == 90 {
+        print("egg is ready")
+    }
+}
+
+
+
